33d1a5dff908c345240d4d7cc789791a6b493d82
arjavsethi <75158301+arjavsethi@users.noreply.github.com>
2022-12-23 19:02:38 +0530

    second commit

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -6,46 +6,39 @@ window.onload = ()=>{
 
 document.onmouseover = function(e: MouseEvent) {
     var element = e.target as HTMLElement; 
-    // console.log(absoluteXPath(element));
-
-
-//  const html = "<div class='hoverText' align=right>" +
-//       "  <span class='imagetest'>"+
-//            " <img src='Files/infoplaceholder.png' height=20 width=20 />"+
-//        "     <span class='hover' > See About Page for more info </span>"+
-//      "   </span>"+
-// "  </div>"
 const html = document.createElement("div");
 html.className = "hoverText";
 html.id ="boosstiElement"
 html.align = "right";
-html.innerHTML = "<span class='imagetest'>"+
-"<img src='https://cdn.imgbin.com/22/15/12/imgbin-minecraft-3x3-pixel-art-font-minecraft-ggSaybW9ed18vQDSkJaQwsFyj.jpg' height=20 width=20 />";
+html.innerHTML = "<span class='imagetest' id='boosstiSpan'>"+
+"<img src='https://cdn.imgbin.com/22/15/12/imgbin-minecraft-3x3-pixel-art-font-minecraft-ggSaybW9ed18vQDSkJaQwsFyj.jpg' height=30 width=30 />";
 html.style.position = "absolute"
-
-
-
-element.appendChild(html);
-
-
-
-
-
-
-
-
-
-
-
+var elDistanceToTop = window.pageYOffset + element.getBoundingClientRect().top
+var elDistanceToLeft = window.pageXOffset + element.getBoundingClientRect().left
+html.style.left = (elDistanceToLeft+10).toString() + "px";
+// console.log(elDistanceToLeft)
+html.style.top = elDistanceToTop.toString()+ "5px";
+html.style.cursor = "pointer";
+html.style.zIndex ="8675309 !important"
+html.style.backgroundColor = "black"
+html.style.height = "30px"
+html.style.width = "30px"
+// html.style.pointerEvents = "none" ;
+document.body.appendChild(html);
 
 }
-document.onmouseout = function(e: MouseEvent) {
-    // var element = e.target as HTMLElement; 
-    // console.log(absoluteXPath(element));
-  
+document.onmouseout = function(e: MouseEvent) {  
     var boosstiElement = document.getElementById('boosstiElement')
+   if(boosstiElement){
     boosstiElement.remove()
+   }
 }
+document.addEventListener("click",(e)=>{
+    var element = e.target as HTMLElement; 
+   if(element.id === "boosstiSpan"|| element.id=="boosstiElement"){
+   console.log("clicked")
+   }
+})
 
 
 
